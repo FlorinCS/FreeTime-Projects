fd6f6b1c40567a74c8b426070e5c7ccb35f06910
FlorinCS <81389602+FlorinCS@users.noreply.github.com>
2022-02-04 21:10:02 +0100

    Add files via upload

---
+++ Porfolio/index.js
@@ -0,0 +1,22 @@
+first=document.getElementById("first")
+second=document.getElementById("sec")
+third=document.getElementById("third")
+text=document.getElementById("text")
+minus=document.getElementById("minus")
+
+first.onmouseover = function(){
+
+
+    text.innerHTML="HOME"
+};
+second.onmouseover = function(){
+
+
+    text.innerHTML="ABOUT"
+};
+third.onmouseover = function(){
+
+
+    text.innerHTML="CONTACT"
+};
+
